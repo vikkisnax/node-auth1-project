01fc6d9d40cb2c2ad8d3433a645430f729785b94
Victoria Blancas <vikki6688@gmail.com>
2023-10-26 18:09:06 -0400

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,18 +18,25 @@ function findBy(filter) {
     // ex: {username: 'foo'} 
 }
 
+
+//check this against his code
+
 /**
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-  
+  return db('users')
+    .where({user_id})
+    .first()
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
-function add(user) {
-
+async function add(user) {
+  const [user_id] = await db('users')
+    .insert(user)
+  return findById(user_id)
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
