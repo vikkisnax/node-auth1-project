b7e4d4fa7d3c2b81631b39317825e2322073e5bb
Victoria Blancas <vikki6688@gmail.com>
2023-10-26 18:00:01 -0400

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,22 +1,28 @@
+//db wrapper
+const db = require('../../data/db-config')
+
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-
+  return db('users')
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
-
+  return db('users')
+    .where(filter) 
+    // filter might be an object w some data we want to inject
+    // ex: {username: 'foo'} 
 }
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-
+  
 }
 
 /**
@@ -27,3 +33,9 @@ function add(user) {
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add
+}
